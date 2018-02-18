667e56f37f3be9e09d2cee2d747119951947206c
Roger <rog65@hotmail.com>
2018-02-18 01:06:19 +0000

    Abstract methods are now implemented in enum declarations

--- src/main/java/com/rogeraderogba/martianrobots/enums/Command.java
+++ src/main/java/com/rogeraderogba/martianrobots/enums/Command.java
@@ -3,11 +3,48 @@ package com.rogeraderogba.martianrobots.enums;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.rogeraderogba.martianrobots.MartianRobotsException;
 import com.rogeraderogba.martianrobots.location.Point;
 import com.rogeraderogba.martianrobots.location.PointOrientation;
 
 public enum Command {
-	LEFT('L'), RIGHT('R'), FORWARD('F');
+	LEFT('L'){
+		public PointOrientation getNextPointOrientation(PointOrientation pointOrientation) {
+			Point newPoint = pointOrientation.getPoint();;
+			Orientation newOrientation = beforeToAfterLeftCommandMap.get(pointOrientation.getOrientation());
+			return new PointOrientation(newPoint, newOrientation);
+		}
+	}, 
+	RIGHT('R'){
+		public PointOrientation getNextPointOrientation(PointOrientation pointOrientation) {
+			Point newPoint = pointOrientation.getPoint();;
+			Orientation newOrientation = beforeToAfterRightCommandMap.get(pointOrientation.getOrientation());
+			return new PointOrientation(newPoint, newOrientation);
+		}
+	}, 
+	FORWARD('F'){
+		public PointOrientation getNextPointOrientation(PointOrientation pointOrientation) {
+			Orientation newOrientation = pointOrientation.getOrientation();
+			int x = pointOrientation.getPoint().getX();
+			int y = pointOrientation.getPoint().getY();
+			switch(pointOrientation.getOrientation()) {
+				case NORTH :
+					y += 1;
+					break;
+				case EAST :
+					x += 1;
+					break;
+				case SOUTH :
+					y -= 1;
+					break;
+				case WEST :
+					x -= 1;
+					break;
+			}
+			Point newPoint = new Point(x,y);
+			return new PointOrientation(newPoint, newOrientation);
+		}	
+	};
 	
 	private static final Map<Orientation, Orientation> beforeToAfterLeftCommandMap;
 	private static final Map<Orientation, Orientation> beforeToAfterRightCommandMap;
@@ -33,42 +70,8 @@ public enum Command {
 	public char getCode() {
 		return this.code;
 	}
-
-	public PointOrientation getNextPointOrientation(PointOrientation pointOrientation) {
-		Point newPoint = null;
-		Orientation newOrientation = null;
-		switch(this) {
-			case LEFT:
-				newOrientation = beforeToAfterLeftCommandMap.get(pointOrientation.getOrientation());
-				newPoint = pointOrientation.getPoint();
-				break;
-			case RIGHT:
-				newOrientation = beforeToAfterRightCommandMap.get(pointOrientation.getOrientation());
-				newPoint = pointOrientation.getPoint();
-				break;
-			case FORWARD:
-				newOrientation = pointOrientation.getOrientation();
-				int x = pointOrientation.getPoint().getX();
-				int y = pointOrientation.getPoint().getY();
-				switch(pointOrientation.getOrientation()) {
-					case NORTH :
-						y += 1;
-						break;
-					case EAST :
-						x += 1;
-						break;
-					case SOUTH :
-						y -= 1;
-						break;
-					case WEST :
-						x -= 1;
-						break;
-				}
-				newPoint = new Point(x,y);
-				break;
-		}
-		return new PointOrientation(newPoint, newOrientation);
-	}
+	
+	public abstract PointOrientation getNextPointOrientation(PointOrientation pointOrientation); 
 	
 	public static Command getCommand(char c) {
 		Command result = null;
@@ -77,6 +80,9 @@ public enum Command {
 				result = command;
 				break;
 			}
+		}
+		if (result == null) {
+			throw new MartianRobotsException("Command currently not supported");
 		}
 		return result;
 	}
